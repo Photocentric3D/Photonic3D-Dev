f7be6f4d959dd4a9c8446ace564835217359f836
WGAndrew <andrew@whisperinggibbon.com>
2016-10-17 11:51:46 +0100

    Cleaned, commented, fixed an interactive installer section and ensured setup script uses photocentric branch of photonic
    Cleaned out some bits.
    Commented some not-obvious choices
    fixed the kweb installer since it annoyingly prompts for a 'Y/N' answer
    changed how we update photonic3d to use the start.sh from this branch rather than the main.

--- host/bin/rpi-setup.sh
+++ host/bin/rpi-setup.sh
@@ -21,26 +21,31 @@ sudo rpi-update
 if [[ "[ "$newhost" == "4kscreen" ]" || "[ "$newhost" == "LCHR" ]" || "[ "$newhost" == "standalone" ]" ]]
 	then
 		echo "update photonic"
-# would prefer to call this to update to a particular version,
-# but the auto-update in start.sh will flatten updates made in the next section
-# so doing this first.
+		# would prefer to call this to update to a particular version,
+		# but the auto-update in start.sh will flatten any changes we make in the next section
+		# so doing this first.
+		sudo chmod +x photonic-repo/host/bin/*.sh
+		# kill the old version of photonic using its stop.sh script
 		sudo /opt/cwh/stop.sh
-		sudo /opt/cwh/start.sh
+		# launch the Photocentric rather than area515 version of start.sh - ensures using Photocentric branch
+		sudo photonic-repo/host/bin/start.sh
 fi
 
 
 # redirect boot terminal output not to screen
 echo "removing pi branding"
+# we can't remove everything as some is baked into the pi's firmware, but this gives a realistic amount.
 sudo mv /boot/cmdline.txt /boot/cmdline.old 
 sudo sh -c 'echo -n "loglevel=3 logo.nologo " > /boot/cmdline.txt'
-sudo sh -c 'cat /boot/cmdline.old >> /boot/cmdline.txt'
+sudo sh -c 'cat /boot/cmdline.old >> /boot/cmdline.txt' 
 sudo sed -i "s/tty1/tty/g" /boot/cmdline.txt
 
 echo "installing common files"
 sudo rsync -avr photonic-repo/host/common/ /
-sudo rsync -avr photonic-repo/host/resourcesnew/printflow /opt/cwh/resourcesnew/ #keep printflow without the 
-sudo cp photonic-repo/host/os/Linux/armv61/pdp /opt/cwh/os/Linux/armv61/pdp
-sudo cp photonic-repo/host/resourcesnew/printflow/holdingpage.html /home/pi/holdingpage.html
+sudo rsync -avr photonic-repo/host/resourcesnew/printflow /opt/cwh/resourcesnew/ #keep printflow without the trailing / 
+sudo cp photonic-repo/host/os/Linux/armv61/pdp /opt/cwh/os/Linux/armv61/pdp #copy display manager for screen + curing screen printers
+sudo cp photonic-repo/host/resourcesnew/printflow/holdingpage.html /home/pi/holdingpage.html #copy holdingpage for fallback
+#install splash screen
 cp /etc/splash.png ~/.splash.png
 sudo chown root /etc/splash.png
 sudo chmod 777 /etc/splash.png
@@ -55,7 +60,7 @@ if [ -e "/etc/default/nodm" ]
 	else
 		echo "nodm doesn't exist"
 fi
-#this already happens once due to our rsync of an X11 config, but why not just make sure, eh?
+# this already happens once due to our rsync of an X11 config, but why not just make sure, eh?
 
 echo "Working on per printer settings..."
 sudo sh -c 'echo \#Photocentric mods >> /boot/config.txt'
@@ -68,16 +73,16 @@ if [[ "[ "$newhost" == "4ktouch" ]" || "[ "$newhost" == "LCHR" ]" || "[ "$newhos
 		sudo sh -c 'echo lcd_rotate=2 >> /boot/config.txt'
 		sudo sh -c 'echo avoid_warnings=1 >> /boot/config.txt'
 		echo "installing kiosk browser"
-		#wget http://steinerdatenbank.de/software/kweb-1.7.4.tar.gz
-		#tar -xzf kweb-1.7.4.tar.gz
-		#cd kweb-1.7.4
-		#./debinstall
-		#rm -rf kweb-1.7.4
+		# since this isn't on standard apt-get sources, i'm keeping this as an alternative way to source kweb
+		###wget http://steinerdatenbank.de/software/kweb-1.7.4.tar.gz
+		###tar -xzf kweb-1.7.4.tar.gz
+		###cd kweb-1.7.4
+		###./debinstall
+		###rm -rf kweb-1.7.4
 		wget -qO - http://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
 		sudo sh -c 'echo "deb http://dl.bintray.com/kusti8/chromium-rpi jessie main" | sudo tee -a /etc/apt/sources.list'
-		sudo apt-get update && sudo apt-get install -y youtube-dl
-		#HACK: installing youtube-dl just because it causes kweb installation script to ask a question that must be replied to on keyboard otherwise
-		sudo apt-get install -y kweb
+		sudo sh -c "yes Y | apt-get install -y kweb"
+		
 		
 		echo "Setting up kiosk-only mode"
 		touch /home/pi/.xsession
