b2244cdf0c5ea43db1d7c7ecabad7ab9e4e9881b
WGAndrew <andrew@whisperinggibbon.com>
2016-10-28 16:16:33 +0100

    RepoConfig for master repo - ready for when needed

---
+++ host/bin/repoconfig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# store per-repo settings and variables in here
+# then use:
+# source repoconfig.sh
+# to import them to different scripts
+
+cpu=`uname -m`
+
+if [ -z "$HOME" ] || [ "$HOME" == "/" ]; then
+  HOME=~root
+fi
+
+DEFAULT_REPO="Photocentric3D/Photonic3D"
+DEV_REPO="Photocentric3D/Photonic3D-Dev"
+TESTKIT_REPO="Photocentric3D/Photonic3D-Dev"
+TESTKITDEV_REPO="Photocentric3D/Photonic3D-Dev"
+
+CONFIG_PROPS="${HOME}/3dPrinters/config.properties"
+
+PHOTOCENTRIC_PORTNO=9091
+PHOTOCENTRIC_PASSWD=photocentric
