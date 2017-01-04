8b493a30c100d58c91e08456317624d860bc5bef
Andrew Howat <andrew@whisperinggibbon.com>
2017-01-04 11:52:34 +0000

    Merge: oneTimeInstall updates
    installs imagemagick as needed, and grabs the updating images

--- host/bin/oneTimeInstall.sh
+++ host/bin/oneTimeInstall.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+# this file is for changes that need to happen to the pi outside of /opt/cwh
+# each one is suggested to be of the structure
+# COMMENT: VERSION this change was first needed from appeared at [in case of updates being applied spanning multiple previous releases]
+# TEST to see if change has already been applied [Optional, heavily dependendent on if the change is destructive]
+# CHANGE script to modify the installation
 
+# version 0.30?
 sed -i "s/KJ/KEJ/g" /home/pi/.xsession
+
+# version 0.31
+if [ ! -e /usr/bin/convert ]; then
+	apt-get install --yes imagemagick
+fi
+
+# version 0.31
+if [ ! -e /etc/updatingBW.png ]; then
+	sudo wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/common/etc/updatingBW.png
+	sudo mv updatingBW.png /etc/
+fi
+# version 0.31
+if [ ! -e /etc/updating.png ]; then
+	sudo wget https://raw.githubusercontent.com/Photocentric3D/Photonic3D/master/host/common/etc/updating.png
+	sudo mv updating.png /etc/
+fi
+
+# version ...
